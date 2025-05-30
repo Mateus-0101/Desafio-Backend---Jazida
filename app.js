71ca334c8f4b25e126f295a07580f0de8f27b989
Mateus Antunes Dias Rodrifues <61789289+Mateus-0101@users.noreply.github.com>
2025-05-30 11:20:44 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+// app.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let items = []; 
+let proxId = 1; 
+
+const tiposPermitidos = ['charizard', 'mewtwo', 'pikachu'];
+
+// C (Create) - Criar um novo item
+app.post('/items', (req, res) => {
+    const { nome, tipo, treinador } = req.body; 
+    
+    if (!nome || !tipo || !treinador) {
+        return res.status(400).json({ message: 'Todos os campos são obrgatórios.' });
+    }
+    
+    const newItem = { id: nextId++, 
+        nome, 
+        tipo,
+        treinador
+    };
+    
+    items.push(newItem);
+    res.status(200).json(newItem); 
+});
